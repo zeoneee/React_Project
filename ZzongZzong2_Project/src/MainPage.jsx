4106f81c0954a0c17b3da240ff0cdb822a9ae4f2
zeoneee <jiwlgus048@gmail.com>
2023-05-13 20:21:57 +0900

    0513: mainpage layout

---
+++ ZzongZzong2_Project/src/MainPage.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import './MainPage.css'
+
+function MainPage() {
+
+    return (
+        <div id="app">
+            <header class="header-comp">
+                <div class="header-inner">
+                    <div class="global-menu-area">
+                        <div class="logo">logo</div>
+                        <div class="menu">
+                            <nav>
+                                <ul>
+                                    <li class>홈 피드</li>
+                                    <li class="is--active">메인 프로젝트</li>
+                                    <li class>팀원 소개</li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div tabindex="-1" class="sub-menu-area">
+                        <div class="sub-menu">sub-menu</div>
+                    </div>
+                </div>
+            </header>
+
+            <div class="main-banner-comp">
+                <div class="main-banner-inner">
+                    <div class="title-area">title-area</div>
+                    <div class="desc-area">desc-area</div>
+                    <div class="bg-area">bg-area</div>
+                </div>
+            </div>
+
+            <div class="apply-grid-system">
+                <div class="layout index main">
+                    <div class="content-comp side-menu">
+                        <div class="content-inner">
+                            <div class="grid-container">
+                                <div class="nav-grid-wrapper">네비게이션 바</div>
+                                <div class="content-area">
+                                    {/* <div class="head-wrap">content wrap</div> */}
+                                    <div class="item-title-area">item title area</div>
+                                    <div class="diary">diary</div>
+                                    <div class="vocab">voca book</div>
+                                    <div class="lecture-note">강의 노트</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-overlay"></div>
+        </div>
+    )
+}
+
+export default MainPage;
--- ZzongZzong2_Project/src/main.jsx
+++ ZzongZzong2_Project/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import GoogleDriveFolderPage from './GoogleDriveFolderPage.jsx'
+
 import './index.css'
+import MainPage from './MainPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <GoogleDriveFolderPage/> */}
+    <MainPage/>
   </React.StrictMode>,
 )
