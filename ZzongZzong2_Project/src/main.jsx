3d57a43250e2f684efc21dfe13b7badf3729a43d
zeoneee <jiwlgus048@gmail.com>
2023-05-16 22:14:13 +0900

    0516: connect page

--- ZzongZzong2_Project/src/main.jsx
+++ ZzongZzong2_Project/src/main.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import GoogleDriveFolderPage from './GoogleDriveFolderPage.jsx'
+import {BrowserRouter} from "react-router-dom"
 
 import './index.css'
 import MainPage from './MainPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <GoogleDriveFolderPage/> */}
-    <MainPage/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
