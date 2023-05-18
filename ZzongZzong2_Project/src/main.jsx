9d052cb1d2284849553955884156ee1b9ae5aa91
Charles <59673870+Charles-min@users.noreply.github.com>
2023-05-18 22:08:42 +0900

    05.18

--- ZzongZzong2_Project/src/main.jsx
+++ ZzongZzong2_Project/src/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import GoogleDriveFolderPage from './GoogleDriveFolderPage.jsx'
 
 import './index.css'
-import MainPage from './MainPage.jsx'
+import LectureNote from './LectureNote.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <GoogleDriveFolderPage/> */}
-    <MainPage/>
+    <LectureNote/>
   </React.StrictMode>,
 )
