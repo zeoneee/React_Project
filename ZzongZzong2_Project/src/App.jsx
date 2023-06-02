a357574b2c907ac8fbae0e07925ba78981dec08f
Charles <59673870+Charles-min@users.noreply.github.com>
2023-06-02 17:22:46 +0900

    06.02 보드 구성

--- ZzongZzong2_Project/src/App.jsx
+++ ZzongZzong2_Project/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 
 import HeaderBar from './HeaderBar'
-// import LandingPage from './LandingPage'
+import LandingPage from './LandingPage'
 import MainPage from './MainPage'
 import MemberPage from './MemberPage'
 import LectureNote from './LectureNote'
@@ -15,7 +15,7 @@ function App() {
     <>
       <HeaderBar />
       <Routes>
-        {/* <Route path='/' element={<LandingPage/>}/> */}
+        <Route path='/' element={<LandingPage/>}/>
         <Route path='/MainPage' element={<MainPage/>}/>
         <Route path='/MemberPage' element={<MemberPage/>}/>
         <Route path='/LectureNote' element={<LectureNote/>}/>
--- ZzongZzong2_Project/src/HeaderBar.jsx
+++ ZzongZzong2_Project/src/HeaderBar.jsx
@@ -6,15 +6,15 @@ function HeaderBar() {
 
     return (
         <div id="header-app">
-            <header class="header-comp">
-                <div class="header-inner">
-                    <div class="global-menu-area">
-                        <div class="logo">logo</div>
-                        <div class="menu">
+            <header className="header-comp">
+                <div className="header-inner">
+                    <div className="global-menu-area">
+                        <div className="logo">logo</div>
+                        <div className="menu">
                             <nav>
                                 <ul>
                                     <Link to="/">
-                                        <li class="is--active">홈 피드</li>
+                                        <li className="is--active">홈 피드</li>
                                     </Link>
                                     <Link to="/MainPage">
                                         <li>메인 프로젝트</li>
